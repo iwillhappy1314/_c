26e697b09dd98ca3d0b571f8f1d5a4a63ac4aa1a
Amos Lee <iwillhappy1314@gmail.com>
2022-08-03 00:50:06 +0800

    前端流程优化

--- frontend/dist/scripts/main.js
+++ frontend/dist/scripts/main.js
@@ -10,7 +10,7 @@
 "use strict";
 
 
-__webpack_require__(/*! script-loader!./plugins/lazyYT */ "./node_modules/script-loader/index.js!./assets/scripts/plugins/lazyYT.js");
+__webpack_require__(/*! script-loader!./plugins/lazyYT */ "./node_modules/.pnpm/script-loader@0.7.2/node_modules/script-loader/index.js!./assets/scripts/plugins/lazyYT.js");
 
 (function ($) {
   var header = document.getElementById('masthead');
@@ -434,20 +434,20 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
-/***/ "./node_modules/raw-loader/index.js!./node_modules/babel-loader/lib/index.js??ruleSet[1].rules[4].use[0]!./assets/scripts/plugins/lazyYT.js":
-/*!**************************************************************************************************************************************************!*\
-  !*** ./node_modules/raw-loader/index.js!./node_modules/babel-loader/lib/index.js??ruleSet[1].rules[4].use[0]!./assets/scripts/plugins/lazyYT.js ***!
-  \**************************************************************************************************************************************************/
+/***/ "./node_modules/.pnpm/raw-loader@0.5.1/node_modules/raw-loader/index.js!./node_modules/.pnpm/babel-loader@8.2.5_xc6oct4hcywdrbo4ned6ytbybm/node_modules/babel-loader/lib/index.js??ruleSet[1].rules[4].use[0]!./assets/scripts/plugins/lazyYT.js":
+/*!*******************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/.pnpm/raw-loader@0.5.1/node_modules/raw-loader/index.js!./node_modules/.pnpm/babel-loader@8.2.5_xc6oct4hcywdrbo4ned6ytbybm/node_modules/babel-loader/lib/index.js??ruleSet[1].rules[4].use[0]!./assets/scripts/plugins/lazyYT.js ***!
+  \*******************************************************************************************************************************************************************************************************************************************************/
 /***/ ((module) => {
 
 module.exports = "/*!\n* lazyYT (lazy load YouTube videos)\n* v1.0.1 - 2014-12-30\n* (CC) This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License.\n* http://creativecommons.org/licenses/by-sa/4.0/\n* Contributors: https://github.com/tylerpearson/lazyYT/graphs/contributors || https://github.com/daugilas/lazyYT/graphs/contributors\n* \n* Usage: <div class=\"lazyYT\" data-youtube-id=\"laknj093n\" data-parameters=\"rel=0\">loading...</div>\n*/\n;\n\n(function ($) {\n  'use strict';\n\n  function setUp($el, settings) {\n    var width = $el.data('width'),\n        height = $el.data('height'),\n        ratio = $el.data('ratio') ? $el.data('ratio') : settings.default_ratio,\n        id = $el.data('youtube-id'),\n        padding_bottom,\n        innerHtml = [],\n        $thumb,\n        thumb_img,\n        loading_text = $el.text() ? $el.text() : settings.loading_text,\n        youtube_parameters = $el.data('parameters') || '';\n    ratio = ratio.split(\":\"); // width and height might override default_ratio value\n\n    if (typeof width === 'number' && typeof height === 'number') {\n      $el.width(width);\n      padding_bottom = height + 'px';\n    } else if (typeof width === 'number') {\n      $el.width(width);\n      padding_bottom = width * ratio[1] / ratio[0] + 'px';\n    } else {\n      width = $el.width(); // no width means that container is fluid and will be the size of its parent\n\n      if (width == 0) {\n        width = $el.parent().width();\n      }\n\n      padding_bottom = ratio[1] / ratio[0] * 100 + '%';\n    } //\n    // This HTML will be placed inside 'lazyYT' container\n\n\n    innerHtml.push('<div class=\"ytp-thumbnail\">'); // Play button from YouTube (exactly as it is in YouTube)\n\n    innerHtml.push('<div class=\"ytp-large-play-button\"');\n    if (width <= 640) innerHtml.push(' style=\"transform: scale(0.563888888888889);\"');\n    innerHtml.push('>');\n    innerHtml.push('<svg>');\n    innerHtml.push('<path fill-rule=\"evenodd\" clip-rule=\"evenodd\" fill=\"#1F1F1F\" class=\"ytp-large-play-button-svg\" d=\"M84.15,26.4v6.35c0,2.833-0.15,5.967-0.45,9.4c-0.133,1.7-0.267,3.117-0.4,4.25l-0.15,0.95c-0.167,0.767-0.367,1.517-0.6,2.25c-0.667,2.367-1.533,4.083-2.6,5.15c-1.367,1.4-2.967,2.383-4.8,2.95c-0.633,0.2-1.316,0.333-2.05,0.4c-0.767,0.1-1.3,0.167-1.6,0.2c-4.9,0.367-11.283,0.617-19.15,0.75c-2.434,0.034-4.883,0.067-7.35,0.1h-2.95C38.417,59.117,34.5,59.067,30.3,59c-8.433-0.167-14.05-0.383-16.85-0.65c-0.067-0.033-0.667-0.117-1.8-0.25c-0.9-0.133-1.683-0.283-2.35-0.45c-2.066-0.533-3.783-1.5-5.15-2.9c-1.033-1.067-1.9-2.783-2.6-5.15C1.317,48.867,1.133,48.117,1,47.35L0.8,46.4c-0.133-1.133-0.267-2.55-0.4-4.25C0.133,38.717,0,35.583,0,32.75V26.4c0-2.833,0.133-5.95,0.4-9.35l0.4-4.25c0.167-0.966,0.417-2.05,0.75-3.25c0.7-2.333,1.567-4.033,2.6-5.1c1.367-1.434,2.967-2.434,4.8-3c0.633-0.167,1.333-0.3,2.1-0.4c0.4-0.066,0.917-0.133,1.55-0.2c4.9-0.333,11.283-0.567,19.15-0.7C35.65,0.05,39.083,0,42.05,0L45,0.05c2.467,0,4.933,0.034,7.4,0.1c7.833,0.133,14.2,0.367,19.1,0.7c0.3,0.033,0.833,0.1,1.6,0.2c0.733,0.1,1.417,0.233,2.05,0.4c1.833,0.566,3.434,1.566,4.8,3c1.066,1.066,1.933,2.767,2.6,5.1c0.367,1.2,0.617,2.284,0.75,3.25l0.4,4.25C84,20.45,84.15,23.567,84.15,26.4z M33.3,41.4L56,29.6L33.3,17.75V41.4z\"></path>');\n    innerHtml.push('<polygon fill-rule=\"evenodd\" clip-rule=\"evenodd\" fill=\"#FFFFFF\" points=\"33.3,41.4 33.3,17.75 56,29.6\"></polygon>');\n    innerHtml.push('</svg>');\n    innerHtml.push('</div>'); // end of .ytp-large-play-button\n\n    innerHtml.push('</div>'); // end of .ytp-thumbnail\n    // Video title (info bar)\n\n    innerHtml.push('<div class=\"html5-info-bar\">');\n    innerHtml.push('<div class=\"html5-title\">');\n    innerHtml.push('<div class=\"html5-title-text-wrapper\">');\n    innerHtml.push('<a id=\"lazyYT-title-', id, '\" class=\"html5-title-text\" target=\"_blank\" tabindex=\"3100\" href=\"//www.youtube.com/watch?v=', id, '\">');\n    innerHtml.push(loading_text);\n    innerHtml.push('</a>');\n    innerHtml.push('</div>'); // .html5-title\n\n    innerHtml.push('</div>'); // .html5-title-text-wrapper\n\n    innerHtml.push('</div>'); // end of Video title .html5-info-bar\n\n    $el.css({\n      'padding-bottom': padding_bottom\n    }).html(innerHtml.join(''));\n\n    if (width > 640) {\n      thumb_img = 'maxresdefault.jpg';\n    } else if (width > 480) {\n      thumb_img = 'sddefault.jpg';\n    } else if (width > 320) {\n      thumb_img = 'hqdefault.jpg';\n    } else if (width > 120) {\n      thumb_img = 'mqdefault.jpg';\n    } else if (width == 0) {\n      // sometimes it fails on fluid layout\n      thumb_img = 'hqdefault.jpg';\n    } else {\n      thumb_img = 'default.jpg';\n    }\n\n    $thumb = $el.find('.ytp-thumbnail').css({\n      'background-image': ['url(//img.youtube.com/vi/', id, '/', thumb_img, ')'].join('')\n    }).addClass('lazyYT-image-loaded').on('click', function (e) {\n      e.preventDefault();\n\n      if (!$el.hasClass('lazyYT-video-loaded') && $thumb.hasClass('lazyYT-image-loaded')) {\n        $el.html('<iframe src=\"//www.youtube.com/embed/' + id + '?autoplay=1&' + youtube_parameters + '\" frameborder=\"0\" allowfullscreen></iframe>').addClass('lazyYT-video-loaded');\n      }\n    });\n    $.getJSON('//gdata.youtube.com/feeds/api/videos/' + id + '?v=2&alt=json', function (data) {\n      $el.find('#lazyYT-title-' + id).text(data.entry.title.$t);\n    });\n  }\n\n  $.fn.lazyYT = function (newSettings) {\n    var defaultSettings = {\n      loading_text: 'Loading...',\n      default_ratio: '16:9',\n      callback: null,\n      // ToDO execute callback if given\n      container_class: 'lazyYT-container'\n    };\n    var settings = $.extend(defaultSettings, newSettings);\n    return this.each(function () {\n      var $el = $(this).addClass(settings.container_class);\n      setUp($el, settings);\n    });\n  };\n})(jQuery);"
 
 /***/ }),
 
-/***/ "./node_modules/script-loader/addScript.js":
-/*!*************************************************!*\
-  !*** ./node_modules/script-loader/addScript.js ***!
-  \*************************************************/
+/***/ "./node_modules/.pnpm/script-loader@0.7.2/node_modules/script-loader/addScript.js":
+/*!****************************************************************************************!*\
+  !*** ./node_modules/.pnpm/script-loader@0.7.2/node_modules/script-loader/addScript.js ***!
+  \****************************************************************************************/
 /***/ ((module) => {
 
 /*
@@ -481,13 +481,13 @@ module.exports = function(src) {
 
 /***/ }),
 
-/***/ "./node_modules/script-loader/index.js!./assets/scripts/plugins/lazyYT.js":
-/*!********************************************************************************!*\
-  !*** ./node_modules/script-loader/index.js!./assets/scripts/plugins/lazyYT.js ***!
-  \********************************************************************************/
+/***/ "./node_modules/.pnpm/script-loader@0.7.2/node_modules/script-loader/index.js!./assets/scripts/plugins/lazyYT.js":
+/*!***********************************************************************************************************************!*\
+  !*** ./node_modules/.pnpm/script-loader@0.7.2/node_modules/script-loader/index.js!./assets/scripts/plugins/lazyYT.js ***!
+  \***********************************************************************************************************************/
 /***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
 
-__webpack_require__(/*! !!./node_modules/script-loader/addScript.js */ "./node_modules/script-loader/addScript.js")(__webpack_require__(/*! !!./node_modules/raw-loader/index.js!./node_modules/babel-loader/lib/index.js??ruleSet[1].rules[4].use[0]!./assets/scripts/plugins/lazyYT.js */ "./node_modules/raw-loader/index.js!./node_modules/babel-loader/lib/index.js??ruleSet[1].rules[4].use[0]!./assets/scripts/plugins/lazyYT.js"))
+__webpack_require__(/*! !!./node_modules/.pnpm/script-loader@0.7.2/node_modules/script-loader/addScript.js */ "./node_modules/.pnpm/script-loader@0.7.2/node_modules/script-loader/addScript.js")(__webpack_require__(/*! !!./node_modules/.pnpm/raw-loader@0.5.1/node_modules/raw-loader/index.js!./node_modules/.pnpm/babel-loader@8.2.5_xc6oct4hcywdrbo4ned6ytbybm/node_modules/babel-loader/lib/index.js??ruleSet[1].rules[4].use[0]!./assets/scripts/plugins/lazyYT.js */ "./node_modules/.pnpm/raw-loader@0.5.1/node_modules/raw-loader/index.js!./node_modules/.pnpm/babel-loader@8.2.5_xc6oct4hcywdrbo4ned6ytbybm/node_modules/babel-loader/lib/index.js??ruleSet[1].rules[4].use[0]!./assets/scripts/plugins/lazyYT.js"))
 
 /***/ })
 
