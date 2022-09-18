74d4716e7ad35e191d45096b86338ec8e6ab50bf
Amos Lee <iwillhappy1314@gmail.com>
2022-09-19 02:17:23 +0800

    Bugfix

--- functions.php
+++ functions.php
@@ -12,9 +12,16 @@ require_once(get_theme_file_path('vendor/autoload.php'));
 
 add_action('wp_enqueue_scripts', function ()
 {
+    wp_enqueue_style('_c-icons', get_theme_file_uri('frontend/dist/styles/iconfont.css'));
     wp_enqueue_style('_c-styles', get_theme_file_uri('frontend/dist/styles/main.css'));
 
     wp_enqueue_script('_c-scripts', get_theme_file_uri('frontend/dist/scripts/main.js'), ['jquery'], '2.0.8');
+    
+    $template_path = parse_url(get_theme_file_uri(), PHP_URL_PATH);
+
+    wp_localize_script('_c-scripts', 'wenpriseSettings', [
+        'staticPath' => $template_path . '/frontend/static/',
+    ]);
 }, 999);
 
 
